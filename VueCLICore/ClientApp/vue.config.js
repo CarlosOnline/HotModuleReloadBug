74a12399240da808d7a999d5a2bcbb5f1178f1bf
Carlos Gomes <cgomes@iinet.com>
2019-02-26 17:54:58 -0800

    vue.config.js

--- VueCLICore/ClientApp/vue.config.js
+++ VueCLICore/ClientApp/vue.config.js
@@ -6,11 +6,6 @@ module.exports={
             args[0].filename = './../../Views/Shared/_Layout.cshtml';
             args[0].template= '../Views/Shared/_LayoutTemplate.cshtml';
             return args;
-			//return[{
-			//	filename: './../../Views/Shared/_Layout.cshtml',
-			//	template: '../Views/Shared/_LayoutTemplate.cshtml'
-			//},
-			//]
 		});
 	}
 }
